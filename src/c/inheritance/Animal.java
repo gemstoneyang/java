2ad5618290fe361c3e23c93016dc37b565c10078
gemstoneyang <bennyws99@gmail.com>
2021-12-26 10:51:19 +0000

    12/26 상속

---
+++ src/c/inheritance/Animal.java
@@ -0,0 +1,14 @@
+package c.inheritance;
+
+public class Animal {
+    String name, kind;
+    int legCount, iq;
+    boolean hasWing;
+    
+    public void move() {
+        System.out.println("Animal move");
+    }
+    public void eatFood() {
+        System.out.println("Animal eat food");
+    }
+}
---
+++ src/c/inheritance/Dog.java
@@ -0,0 +1,7 @@
+package c.inheritance;
+
+public class Dog extends Animal {
+    public void say() {
+        System.out.println("BowWow");
+    }
+}
