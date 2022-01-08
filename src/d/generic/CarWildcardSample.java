7debbe6d54f0338ad885621854c65e5ff9ffbe11
gemstoneyang <bennyws99@gmail.com>
2022-01-08 12:15:22 +0000

    1/8 제네릭

---
+++ src/d/generic/CarWildcardSample.java
@@ -0,0 +1,23 @@
+package d.generic;
+
+public class CarWildcardSample {
+    public static void main(String[] args) {
+        CarWildcardSample sample = new CarWildcardSample();
+        // sample.callBoundedWildCardMethod();
+        sample.callBusBoundedWildcardMethod();
+    }
+    public void callBoundedWildCardMethod() {
+        WildcardGeneric<Car> wildcard = new WildcardGeneric<Car>();
+        wildcard.setWildcard(new Car("Mustang"));
+        boundedWildcardMethod(wildcard);
+    }
+    public void boundedWildcardMethod(WildcardGeneric<? extends Car> c) {
+        Car value = c.getWildcard();
+        System.out.println(value);
+    }
+    public void callBusBoundedWildcardMethod() {
+        WildcardGeneric<Bus> wildcard = new WildcardGeneric<Bus>();
+        wildcard.setWildcard(new Bus("6900"));
+        boundedWildcardMethod(wildcard);
+    }
+}
